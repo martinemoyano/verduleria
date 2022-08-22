840e5095ca8b4f93670b4650f3ccb95b58c3ef58
martinemoyano <memoyano@gmail.com>
2022-08-21 21:22:33 -0300

    modal no funciona

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 let carritoCompras = []
 
 const contenedorProductos = document.getElementById('contenedor-productos');
-const contenedorCarrito = document.getElementById('contenedor-carrito');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
 
 const contadorCarrito = document.getElementById('contadorCarrito');
-const precioTotal = document.getElementById('precioTotal');
+const precioTotal = document.getElementById('precio-total');
 
 const buscador = document.getElementById('search');
 
 
 
 
-
+mostrarProductos(stockProductos)
 
 function mostrarProductos(){
 stockProductos.forEach(item=>{
@@ -22,25 +22,24 @@ stockProductos.forEach(item=>{
                     <div class="card-body">
                     <h5 class="nombreItem">${item.nombre}</h5>
                     <p class="precioItem">$${item.precio}</p>
-                    <a "id=botonAgregar${item.id}" class="btn btn-primary">Agregar</a>
+                    <a id="botonAgregar${item.id}" class="btn btn-primary">Agregar</a>
                    </div>
                    </div>`
 
 contenedorProductos.appendChild(div);
-let btnAgregar = document.getElementById('botonAgregar${item.id}')
-// btnAgregar.addEventListener('click',()=>{
-    // agregarAlCarrito(item.id);
-// })
+let btnAgregar = document.getElementById(`botonAgregar${item.id}`)
+btnAgregar.addEventListener('click',()=>{
+    agregarAlCarrito(item.id);
+})
 })
 }
-mostrarProductos()
 
 
 function agregarAlCarrito(id) {
 let existe = carritoCompras.find(producto => producto.id == id)
 if (existe){
     existe.cantidad = existe.cantidad +1
-    document.getElementById('cant${existe.id}').innerHTML = `<p id = "cant${existe.id}">cantidad:${existe.cantidad}</p>`
+    document.getElementById(`cant${existe.id}`).innerHTML = `<p id = "cant${existe.id}">cantidad:${existe.cantidad}</p>`
     actualizarCarrito()
 }else{
         let productoAgregar = stockProductos.find(item=> item.id == id)
@@ -52,16 +51,33 @@ if (existe){
 }
 
 
+
 function mostrarCarrito(productoAgregar) {
-
     let div = document.createElement('div')
-    div.setAttribute('class', 'productoEnCarrito')
+    div.className="productoEnCarrito";
     div.innerHTML += `<p>${productoAgregar.nombre}</p>
-                    <p>Precio: $${productoAgregar.precio}</p>
-                    <p id="cant${productoAgregar.id}">cantidad:${productoAgregar.cantidad}</p>
-                    <button class="boton-eliminar" id="${productoAgregar.id}">
-                    <i class="fas fa-trash-alt"></i>
-                    </button>`
-    contenedorCarrito.appendChild(div)}
+                      <p>Precio: $${productoAgregar.precio}</p>
+                      <p id="cant${productoAgregar.id}">cantidad:${productoAgregar.cantidad}</p>
+                      <button class="boton-eliminar" id="${productoAgregar.id}">
+                      <i class="fas fa-trash-alt"></i>
+                      </button>`
+    contenedorCarrito.appendChild(div)
+
+}
 
 
+    function actualizarCarrito(){
+        contadorCarrito.innerText = carritoCompras.length
+        precioTotal.innerText = carritoCompras.reduce((acc,el)=>acc + el.precio,0)
+    }
+    
+
+let btnEliminar = document.getElementsByClassName('boton-eliminar')
+for (const btn of btnEliminar){
+    btn.addEventListener('click',()=>{
+
+    })
+}
+
+console.log(contenedorCarrito)
+console.log(carritoCompras)
